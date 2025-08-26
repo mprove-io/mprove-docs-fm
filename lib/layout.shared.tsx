0eea21df83e2caebbbf4e0dcdfb4220a8f6fc487
akalitenya <akalitenya@mprove.io>
2025-08-26 17:36:46 +0500

    d32

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -1,4 +1,5 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import MproveLogo from "./mprove-logo";
 
 /**
  * Shared layout configurations
@@ -12,15 +13,7 @@ export function baseOptions(): BaseLayoutProps {
     nav: {
       title: (
         <>
-          <svg
-            width="24"
-            height="24"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-label="Logo"
-          >
-            <circle cx={12} cy={12} r={12} fill="currentColor" />
-          </svg>
-          Mprove Docs
+          <MproveLogo height="24"></MproveLogo> Mprove Docs
         </>
       ),
     },

c54c1213a6362dd481d075e56fe0d8c684a37dd8
akalitenya <akalitenya@mprove.io>
2025-08-27 10:15:55 +0500

    d34

---
+++ content/docs/parts/filter-conditions-string-yaml.ts
@@ -0,0 +1,25 @@
+export const filterConditionsStringYaml = `apply_to: field_path
+conditions:     ### SQL ###
+- f\`a%z\`        # OR field LIKE 'a%z'
+- f\`-FOO-\`      # OR field = 'FOO'
+- f\`%FOO%\`      # OR field LIKE '%FOO%'
+- f\`FOO%\`       # OR field LIKE 'FOO%'
+- f\`%FOO\`       # OR field LIKE '%FOO'
+- f\`null\`       # OR field IS NULL
+- f\`blank\`      # OR COALESCE(field,'') = ''
+
+- f\`OH, NY\`     # OR field IN ('OH', 'NY')
+- f\`UT, MT%\`    # OR field = 'UT' OR field LIKE 'MT%'
+- f\`\\\\%%\`       # OR field LIKE '\\\\%%'
+
+- f\`-a%z\`       # AND field NOT LIKE 'a%z' OR field IS NULL
+- f\`-FOO\`       # AND field != 'FOO' OR field IS NULL
+- f\`-%FOO%\`     # AND field NOT LIKE '%FOO%' OR field IS NULL
+- f\`-FOO%\`      # AND field NOT LIKE 'FOO%' OR field IS NULL
+- f\`-%FOO\`      # AND field NOT LIKE '%FOO' OR field IS NULL
+- f\`-null\`      # AND field IS NOT NULL
+- f\`-blank\`     # AND COALESCE(field,'') != ''
+
+- f\`-NC, -ND\`   # AND field NOT IN ('NC', 'ND') OR field IS NULL
+
+- f\`\`           # true // the same as no filter`

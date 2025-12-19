17d81acd710d76c60b0539e53c947b16edb2a34b
akalitenya <akalitenya@mprove.io>
2025-12-19 15:01:43 +0500

    d76

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,16 +1,19 @@
-import Link from 'next/link';
+// import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
 export default function HomePage() {
-  return (
-    <div className="flex flex-col justify-center text-center flex-1">
-      <h1 className="mb-4 text-2xl font-bold">Mprove Docs</h1>
-      <p>
-        You can open{' '}
-        <Link href="/docs" className="font-medium underline">
-          /docs
-        </Link>{' '}
-        and see the documentation.
-      </p>
-    </div>
-  );
+  redirect('/docs/quickstart'); 
+  
+  // return (
+  //   <div className="flex flex-col justify-center text-center flex-1">
+  //     <h1 className="mb-4 text-2xl font-bold">Mprove Docs</h1>
+  //     <p>
+  //       You can open{' '}
+  //       <Link href="/docs" className="font-medium underline">
+  //         /docs
+  //       </Link>{' '}
+  //       and see the documentation.
+  //     </p>
+  //   </div>
+  // );
 }

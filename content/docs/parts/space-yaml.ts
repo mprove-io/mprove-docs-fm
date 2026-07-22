987f423f009076f7dcb3b0bc7e3819a1b45e7abb
akalitenya <akalitenya@mprove.io>
2026-07-22 14:28:47 +0500

    d221

---
+++ content/docs/parts/space-yaml.ts
@@ -0,0 +1,15 @@
+export const spaceYaml = `space: space_name
+title: ''
+access_roles:
+- role
+- role
+folders:
+- space: s2
+  access_roles:
+  - role
+  - role
+  folders:
+  - space: s3
+    access_roles: 
+    - role
+    - role`
